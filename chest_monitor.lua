236cb232cf880c4aaff78bae614c0bba64437d7c
Joshua <151913255+JoshuaKool@users.noreply.github.com>
2024-05-14 15:31:53 +0200

    chest_monitor.lua

--- chest_monitor.lua
+++ chest_monitor.lua
@@ -1,28 +1,14 @@
-function print_inventory_on_monitor(chest, monitor)
-    local inventory = chest.list()
-    local y = 1
-    local width, height = monitor.getSize()
+local monitor = peripheral.find("monitor")
+local chest = peripheral.find("minecraft:chest")
 
-    for slot, item in pairs(inventory) do
+print("Setting up missing peripherals...\n")
 
-        item.name = item.name:gsub("minecraft:", "")
-        local text = item.name
-        local text2 = "x" .. item.count
-        local x = math.floor((width - #text) / 2)
-        monitor.setCursorPos(x, y)
-        monitor.write(text)
-        y = y + 1
-        x = math.floor((width - #text2) / 2)
-        monitor.setCursorPos(x, y)
-        monitor.setTextColor(colors.gray)
-        monitor.write(text2)
-        y = y + 1
-
-        monitor.setTextColor(colors.white)
-    end
+if monitor == nil then
+    print("- No monitor found: Attaching monitor to the right side of the computer.")
+    peripheral.create('right', 'monitor')
 end
-local box = peripheral.find("minecraft:chest")
-if box == nil then
+
+if chest == nil then
     print("- No chest found: Attaching chest to the left side of the computer.")
     peripheral.create('left', 'minecraft:chest', false)
 
@@ -31,6 +17,35 @@ if box == nil then
     chest.setItem(1, {name="minecraft:diamond", count=42})
     chest.setItem(2, {name="minecraft:iron_ingot", count=24})
     chest.setItem(3, {name="minecraft:gold_ingot", count=12})
+end
+
+print("\nSetup complete. You should now have a chest on the left side and a monitor on the right side of your computer.\n")
+print("- The chest contains some items for testing purposes.\n")
+print("You can now proceed with the exercise!")
+
+shell.run("peripherals")
+
+function print_inventory_on_monitor(chest, monitor)
+    local inventory = chest.list()
+    local y = 1
+
+    local width, height = monitor.getSize()
+
+    for slot, item in pairs(inventory) do
+        item.name = item.name:gsub("minecraft:", "")
+        local text = item.name
+        local text2 = "x" .. item.count
+        local x = math.floor((width - #text) / 2)
+        monitor.setCursorPos(x, y)
+        monitor.write(text)
+        y = y + 1
+        x = math.floor((width - #text2) / 2)
+        monitor.setTextColor(colors.gray)
+        monitor.setCursorPos(x, y)
+        monitor.write(text2)
+        y = y + 1
+        monitor.setTextColor(colors.white)
+    end
 end
 
 while true do
