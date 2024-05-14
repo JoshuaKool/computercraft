7ebbd8b754d79bd0c0e2c46f1e4dfd329567b602
Joshua <151913255+JoshuaKool@users.noreply.github.com>
2024-05-14 16:31:49 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,17 +1,19 @@
-response = http.get("https://github.com/JoshuaKool/computercraft/blob/main/chest_monitor.lua")
-
-if response then
-    local content = response.readAll()
-    response.close()
-    local file = fs.open("chest_monitor.lua", "w")
-    file.write(content)
-    file.close()
-
-    local startupFile = fs.open("startup.lua", "w")
-    startupFile.write("shell.run('chest_monitor.lua')")
-    startupFile.close()
-
-    shell.run("reboot")
-else
-    print("Failed to download the file. Please try again later.")
+local function download(url, path)
+    local response = http.get(url)
+    if response and response.getResponseCode() == 200 then
+        local content = response.readAll()
+        local file = fs.open(path, "w")
+        file.write(content)
+        file.close()
+        print("File " .. path .. " downloaded successfully.")
+    else
+        print("Failed to download " .. path)
+    end
 end
+
+local function install()
+    download("https://github.com/JoshuaKool/computercraft/blob/main/chest_monitor.lua", "chest_monitor.lua")
+    fs.delete("install.lua")
+end
+
+install()
